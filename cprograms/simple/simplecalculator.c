77023ca85fc952d2e598990208759802e77b9a90
Lakshmi Soujanya Kilaru <lakshmikilaru123@gmail.com>
2021-06-01 18:46:04 +0530

    Create simplecalculator.c
    My first c program.

---
+++ cprograms/simple/simplecalculator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+//Simple Calculator
+#include <stdio.h>
+int main()
+{
+    char operator;
+    float first,second,value;
+    printf("Simple Calculator\n");
+    printf("Enter the operator and two numbers \n");
+    scanf("%c %f %f ", &operator,&first, &second);
+    switch (operator) {
+    case '+': value = first + second;
+              printf("%f %c %f = %f", first,operator, second, value);
+              break;
+    case '-':value = first - second;
+             printf("%f %c %f = %f", first,operator, second,value);
+              break;
+    case '*': value = first * second;
+             printf("%f %c %f = %f", first,operator, second,value);
+             break;
+    case '/': if(second != 0)
+             { value = first/second;
+             printf("%f %c %f = %f", first,operator ,second,value);
+            }
+            else {
+            printf("Divide by zero error");}
+            break;
+    default:
+            printf("Error! operator is not correct");
+            break;
+    }
+
+    return 0;
+}
